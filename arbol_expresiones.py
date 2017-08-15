436f1c9e3375eb702573584e15b752b70b47e936
apdaza <apdaza@gmail.com>
2017-08-15 17:36:08 -0500

    agregdo de arboles de expresiones

---
+++ arbol_expresiones.py
@@ -0,0 +1,33 @@
+from pila import *
+from arbol import *
+
+def convertir(lista, pila):
+    if lista != []:
+        if lista[0] in "+-*/":
+            nodo_der = pila.desapilar()
+            nodo_izq = pila.desapilar()
+            pila.apilar(Nodo(lista[0],nodo_izq,nodo_der))
+        else:
+            pila.apilar(Nodo(lista[0]))
+        return convertir(lista[1:],pila)
+            
+
+def evaluar(arbol):
+    if arbol.valor == "+":
+        return evaluar(arbol.izq) + evaluar(arbol.der)
+    if arbol.valor == "-":
+        return evaluar(arbol.izq) - evaluar(arbol.der)
+    if arbol.valor == "/":
+        return evaluar(arbol.izq) / evaluar(arbol.der)
+    if arbol.valor == "*":
+        return evaluar(arbol.izq) * evaluar(arbol.der)
+    return int(arbol.valor)
+    
+exp = raw_input("ingrese l expresion en posfija: ").split(" ")
+
+pila = Pila()
+
+convertir(exp, pila)
+
+print evaluar(pila.desapilar())
+
